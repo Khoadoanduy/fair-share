d043832eb46fc7e8e5a6317581cead6765ab1899
nnh1125 <ngan_ha.nguyen@tufts.edu>
2025-06-17 21:47:10 -0400

    add endpoint to retrieve list of paymentIntents for a user

--- backend/routes/stripe_payment.ts
+++ backend/routes/stripe_payment.ts
@@ -53,5 +53,27 @@ router.post('/charge-user', async function (request, response) {
         response.status(500).json({err});
       }
   });
+
+router.get('/transactions', async function (request, response) {
+  try {
+      const customerStripeID = request.query.customerStripeID;
+      if (!customerStripeID) {
+          return response.status(400).json({ error: 'Customer Stripe ID is required' });
+      }
+
+      const transactions = await stripe.paymentIntents.list({
+          limit: 20,
+          customer: customerStripeID,
+      });
+
+      response.json(transactions.data);
+  } catch (err) {
+      // Log for debugging
+      console.error('Error fetching Stripe transactions:', err);
+
+      // Send back the Stripe (or other) error message
+      response.status(500).json({ err });
+  }
+});
     
 export default router;
