6309320d6393f9be27ca8053edc76c50df2e3b35
nnh1125 <ngan_ha.nguyen@tufts.edu>
2025-06-24 10:30:27 -0400

    fix charge-user endpoint

--- backend/routes/stripe_payment.ts
+++ backend/routes/stripe_payment.ts
@@ -1,9 +1,4 @@
-// import dotenv from 'dotenv';
 import express, { Request, Response, Router } from 'express';
-
-
-// Initialize environment variables
-// dotenv.config();
 
 // Initialize Stripe with secret key
 const stripe = require('stripe')(
